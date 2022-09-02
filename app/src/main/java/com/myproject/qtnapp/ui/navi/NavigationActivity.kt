9c66bcc8eb09f74da9ba9379993f24570c5861cc
Enrico Maricar <enrico.maricar@gmail.com>
2022-09-02 10:01:41 +0700

    [enrico]base develop

--- app/src/main/java/com/myproject/qtnapp/ui/navi/NavigationActivity.kt
+++ app/src/main/java/com/myproject/qtnapp/ui/navi/NavigationActivity.kt
@@ -1,11 +1,13 @@
-package com.myproject.qtnapp
+package com.myproject.qtnapp.ui.navi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.myproject.qtnapp.R
 
-class MainActivity : AppCompatActivity() {
+class NavigationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_navigation)
     }
 }
